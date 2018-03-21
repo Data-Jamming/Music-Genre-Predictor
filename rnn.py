a97eff7583fdef8e901b88fe6745e0c1748c0d6b
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2018-03-21 15:24:34 -0700

    Documentation and clean up

--- rnn.py
+++ rnn.py
@@ -12,44 +12,48 @@ import torch.nn as nn
 
 
 class rnn(nn.Module):
-    # Describe the shape of the neural net, n_input will be the size of the vector
-    # layers are arrays where each element is an integer that describes the size of that hidden layer
-    # the out is what the NN will say what class it is and the rec is the part that gets "recurred"
-    # to the next iteration of the rnn.
-    # n_classes is the number of things we are classifying.
-    def __init__(self, n_input, out_layers, rec_layers, n_classes):
+    def __init__(self, n_input, out_layers_sizes, rec_layers_sizes, n_classes):
+        """
+        Params:
+            n_input (int): size of the input vector.
+            out_layers_sizes (list): sizes of each hidden layer.
+            rec_layers_sizes (list): sizes of each recurrence layer.
+            n_classes (int): number of classes (music genres, in our case).
+        """
         super(rnn, self).__init__()
         self.n_classes = n_classes
-        self.rec_size = n_classes  # I'm not really sure how big this should be I just picked arbitrarily.
-        #We might want it to be at least the size of n_classes to say how likely it thinks each class is 
-        #but maybe it should try to compress that info?
-        self.n_input = n_input
-        self.softmax = nn.LogSoftmax(dim=0)
-        self.out_layers = []
-        self.out_layers.append(nn.Linear(n_input + self.rec_size, out_layers[0]))
-        for i, n_layer in enumerate(out_layers[:-1]):
-            self.out_layers.append(nn.Linear(n_layer, out_layers[i+1]))
-        self.out_layers.append(nn.Linear(out_layers[-1], self.n_classes))
-        # I'm thinking rec_layers should be really small probably only one layer???
-        self.rec_layers=[]
-        self.rec_layers.append(nn.Linear(n_input + self.rec_size, rec_layers[0]))
-        for i, n_layer in enumerate(rec_layers[:-1]):
-                self.rec_layers.append(nn.Linear(n_layer, rec_layers[i+1]))
 
-        self.rec_layers.append(nn.Linear(rec_layers[-1], self.rec_size))
-        
+        # I'm not really sure how big this should be I just picked arbitrarily.
+        # We might want it to be at least the size of n_classes to say how likely it thinks each class is
+        # but maybe it should try to compress that info?
+        self.rec_size = n_classes
+
+        self.n_input = n_input
+
+        self.out_layers = []
+        self.out_layers.append(nn.Linear(n_input + self.rec_size, out_layers_sizes[0]))
+        for i, n_layer in enumerate(out_layers_sizes[:-1]):
+            self.out_layers.append(nn.Linear(n_layer, out_layers_sizes[i+1]))
+        self.out_layers.append(nn.Linear(out_layers_sizes[-1], self.n_classes))
+
+        # I'm thinking rec_layers_sizes should be really small probably only one layer???
+        self.rec_layers=[]
+        self.rec_layers.append(nn.Linear(n_input + self.rec_size, rec_layers_sizes[0]))
+        for i, n_layer in enumerate(rec_layers_sizes[:-1]):
+            self.rec_layers.append(nn.Linear(n_layer, rec_layers_sizes[i+1]))
+        self.rec_layers.append(nn.Linear(rec_layers_sizes[-1], self.rec_size))
+
+        # we have to register our NN params manually bc we init'd them within the append method
         for i, layer in enumerate(self.out_layers + self.rec_layers):
             self.register_parameter("l" + str(i), layer.weight)
             self.register_parameter("b" + str(i), layer.bias)
 
     def forward(self, input, rec):
         combine=torch.cat((input, rec), 1)
-        
+
         output=F.relu(self.out_layers[0](combine))
         for l in self.out_layers[1:]:
             output=F.tanh(l(output))
-        
-        #output=self.softmax(output)
 
         rec=F.relu(self.rec_layers[0](combine))
         for l in self.rec_layers[1:]:
@@ -57,7 +61,7 @@ class rnn(nn.Module):
 
         #print("output:", output)
         #print("rec:", rec)
-            
+
         return output, rec
 
     def init_rec(self):
--- training.py
+++ training.py
@@ -12,17 +12,17 @@ PRINTERVAL = 1
 class Trainer():
     def __init__(self):
         csv_reader = dataset.load_data("dataset/cleaned_lyrics.csv")
-        
+
         next(csv_reader)
 
-        for i in range(0): #TEMPORARY 
+        for i in range(0): #TEMPORARY
             next(csv_reader)
 
         self.data = []
         self.labels = []
 
         datasize = 1000 #Just make this arbitrarily large when you want to use the whole dataset
-        
+
         print("Loading data...")
         for i in range(datasize):
             try:
@@ -37,12 +37,11 @@ class Trainer():
         self.label_encoder = OHencoder.encode(self.labels)
 
         self.data_decoder = list(self.data_encoder.keys())  #Gives you word/genre from vector index
-        self.label_decoder = list(self.label_encoder.keys()) 
+        self.label_decoder = list(self.label_encoder.keys())
 
         #print([data_enconder[word] for word in data[-1]])
-    
-        self.model = rnn(len(self.data_encoder), [256, 64], [64], len(self.label_encoder))
-    
+        self.model = rnn(len(self.data_encoder), [64, 64], [64, 64], len(self.label_encoder))
+
     def get_pred(self, lyrics):
         rec = self.model.init_rec()
         for word in lyrics:
@@ -51,7 +50,7 @@ class Trainer():
             input[0, self.data_encoder[word]] = 1
             pred, rec = self.model.forward(input, rec)
         return pred
-        
+
     def train(self):
         criterion = nn.CrossEntropyLoss()
         o = torch.optim.SGD(self.model.parameters(), lr = 0.001)
@@ -65,13 +64,13 @@ class Trainer():
             genre = song[1]
             pred = self.get_pred(lyrics)
             y = Variable(torch.LongTensor([self.label_encoder[genre]]))
-            
+
             if i%PRINTERVAL == 0:
                 value, index = torch.max(pred, 1)
                 #print(pred)
                 #print(value)
                 print(str(i)+ ":", "Guessing", self.label_decoder[int(index[0])], "With", float(value[0]), "confidence. Correct genre was", genre + ".")
-            
+
             loss = criterion(pred, y)
             loss.backward()
             o.step()
