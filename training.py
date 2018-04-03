5c5e3fb9d6f3b3ce5affdfa1e20da75fd6fccfc6
Robert <r.craig.email@gmail.com>
2018-04-02 19:53:06 -0700

    able to save models and learning faster now

--- training.py
+++ training.py
@@ -6,9 +6,16 @@ from utils import dataset
 import nltk
 import OHencoder
 import random
+import os
 
 PRINTERVAL = 1
-BATCH_SIZE = 50
+BATCH_SIZE = 1 #For now, keep it as 1 each song is a batch for each word in the song
+MAX_EPOCHS = 1000
+LEARNING_RATE = 0.002
+VAL_RATIO = 0.1
+
+LOG_PATH = "logs/checkpoint.pth"
+SAVE_PATH = "save/best_model.pth"
 
 class Trainer():
     def __init__(self):
@@ -20,7 +27,9 @@ class Trainer():
             next(csv_reader)
 
         self.data = []
+        self.val_data = []
         self.labels = []
+        self.val_labels = []
 
         datasize = 1000 #Just make this arbitrarily large when you want to use the whole dataset
 
@@ -33,6 +42,9 @@ class Trainer():
             except StopIteration:
                 break;
 
+        self.val_data = self.data[int(len(self.data)*(1-VAL_RATIO)):]
+        self.val_labels = self.labels[int(len(self.data)*(1-VAL_RATIO)):]
+                
         print("Building encoder...")
         self.data_encoder = OHencoder.encode(j for i in self.data for j in i)
         self.label_encoder = OHencoder.encode(self.labels)
@@ -40,24 +52,66 @@ class Trainer():
         self.data_decoder = list(self.data_encoder.keys())  #Gives you word/genre from vector index
         self.label_decoder = list(self.label_encoder.keys())
 
+        self.data = self.data[:int(len(self.data)*(1-VAL_RATIO))]
+        self.labels = self.labels[:int(len(self.labels)*(1-VAL_RATIO))]
+        
         #print([data_enconder[word] for word in data[-1]])
-        self.model = rnn(len(self.data_encoder), [1], [16], len(self.label_encoder))
+        self.model = rnn(len(self.data_encoder), [32], [32], len(self.label_encoder))
+        self.best_acc = 0
+        
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr = LEARNING_RATE)
 
     def get_pred(self, lyrics):
         rec = self.model.init_rec()
-        for word in lyrics:
+        preds = Variable(torch.FloatTensor(len(lyrics), self.model.n_classes).zero_())
+        for i, word in enumerate(lyrics):
             #print(word)
             input = Variable(torch.zeros(1, len(self.data_encoder)))
+            
             input[0, self.data_encoder[word]] = 1
             pred, rec = self.model.forward(input, rec)
-        return pred
+            preds[i] = pred
 
+        return preds
+
+    
+    def get_accuracy(self):
+        correct = 0
+        for i, song in enumerate(self.val_data):
+            pred = self.get_pred(song)
+            value, index = torch.max(torch.mean(pred, 0, True), 1)
+            if self.label_encoder[self.val_labels[i]] == int(index):
+                correct += 1
+                
+        acc = correct/len(self.val_data)
+        
+        if acc > self.best_acc:
+            self.best_acc = acc
+            
+        return acc
+    
+    def save_checkpoint(self, epoch, isbest=False, path=LOG_PATH):
+        torch.save({
+            'epoch':epoch,
+            'state_dict':self.model.state_dict(),
+            'best_acc':self.best_acc,
+            'optimizer':self.optimizer.state_dict()
+        }, path)
+        if isbest:
+            torch.save({
+                'epoch':epoch,
+                'state_dict':self.model.state_dict(),
+                'best_acc':self.best_acc,
+                'optimizer':self.optimizer.state_dict()
+        }, SAVE_PATH)
+        
     def train(self):
-        criterion = nn.CrossEntropyLoss()
-        o = torch.optim.SGD(self.model.parameters(), lr = 0.001)
+
         #for param in self.model.parameters():
-        #    print(param.data, param.size)
-        o.zero_grad()
+        #    print(param.size())
+        
+        
         songs = [[self.data[i], self.labels[i]] for i in range(len(self.data))]
         random.shuffle(songs)
         preds = Variable(torch.FloatTensor(BATCH_SIZE, len(self.label_encoder)).zero_())
@@ -66,32 +120,43 @@ class Trainer():
             lyrics = song[0]
             genre = song[1]
             pred = self.get_pred(lyrics)
-            labels[i%BATCH_SIZE] = self.label_encoder[genre]
+            labels = Variable(torch.LongTensor([self.label_encoder[genre] for i in pred]))
 
-            preds[i%BATCH_SIZE] = pred
+            preds = pred
             
-            if i%PRINTERVAL == 0:
-                value, index = torch.max(pred, 1)
-                #print(pred)
-                #print(value)
+            if (i+1)%PRINTERVAL == 0:
+                value, index = torch.max(torch.mean(pred, 0, True), 1)
+                #print(preds[i])
+                #print(labels[i])
                 print(str(i)+ ":", "Guessing", self.label_decoder[int(index[0])], "With", float(value[0]), "confidence. Correct genre was", genre + ".")
 
             if (i+1)%BATCH_SIZE == 0 or i >= len(songs)-1:
-                print(0)
-                loss = criterion(preds, labels)
-                print(1)
+                self.loss = self.criterion(preds, labels)
+                
+                self.optimizer.zero_grad()
 
-                loss.backward()
-                print(2)
+                self.loss.backward()
 
-                o.step()
-                print(3)
-
+                self.optimizer.step()
                 preds = Variable(torch.FloatTensor(BATCH_SIZE, len(self.label_encoder)).zero_())
                 labels = Variable(torch.LongTensor(BATCH_SIZE).zero_())
+                
+trainer = Trainer()    
+epoch_start = 0
+if os.path.isfile(LOG_PATH):
+    print("Checkpoint found! Resuming...")
+    checkpoint = torch.load(LOG_PATH)
+    epoch_start = checkpoint['epoch']
+    trainer.model.load_state_dict(checkpoint['state_dict'])
+    trainer.optimizer.load_state_dict(checkpoint['optimizer'])
+    trainer.best_acc = checkpoint['best_acc']
+    
 
-trainer = Trainer()
-trainer.train()
-
+for i in range(epoch_start, MAX_EPOCHS):
+    print("\nEpoch", i+1)
+    trainer.train()
+    acc = trainer.get_accuracy()
+    print("\nLoss:",trainer.loss.data[0],"\nAccuracy:", acc)
+    trainer.save_checkpoint(i+1, isbest=(acc == trainer.best_acc))
 
 
