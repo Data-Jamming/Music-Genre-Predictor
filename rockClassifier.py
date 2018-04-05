809db2661d2ee32a6634d100c83e43443f7750f3
Jordan Jay <jordanjay28@gmail.com>
2018-04-05 00:13:31 -0700

    Updated rockClassifier to count all genres within the nl_features.csv

--- rockClassifier.py
+++ rockClassifier.py
@@ -8,13 +8,24 @@ import csv
 # The Genre of the song should always be listed at index 1 of the row
 def main():
 	# path = os.getcwd() + "/dataset/cleaned_lyrics.csv"
-	path = os.getcwd() + "/dataset/nl_features_subset.csv"
+	path = os.getcwd() + "/dataset/nl_features.csv"
 	csv_reader = dataset.load_data(path)
 
 	rock_count = 0
 	not_rock_count = 0
 	NA_count = 0
 	other_count = 0
+	total_count = 0
+
+	country_count = 0
+	electronic_count = 0
+	folk_count = 0
+	indie_count = 0
+	jazz_count = 0
+	metal_count = 0
+	pop_count = 0
+	rb_count = 0
+	hiphop_count = 0
 
 	for line in csv_reader:
 		if len(line) == 0:
@@ -22,8 +33,27 @@ def main():
 		if "genre" in line or "annotations" in line:
 			continue
 
+		total_count += 1
 		if line[1] == "Rock":
 			rock_count += 1
+		elif line[1] == "Country":
+			country_count += 1
+		elif line[1] == "Electronic":
+			electronic_count += 1
+		elif line[1] == "Hip-Hop":
+			hiphop_count += 1
+		elif line[1] == "Folk":
+			folk_count += 1
+		elif line[1] == "Indie":
+			indie_count += 1
+		elif line[1] == "Jazz":
+			jazz_count += 1
+		elif line[1] == "Metal":
+			metal_count += 1
+		elif line[1] == "Pop":
+			pop_count += 1
+		elif line[1] == "R&B":	
+			rb_count += 1
 		elif line[1] == "Not Available":
 			NA_count += 1
 		elif line[1] == "Other":
@@ -32,6 +62,16 @@ def main():
 			not_rock_count += 1
 
 	print("Rock count is : " + str(rock_count))
+	print("Country count is : " + str(country_count))
+	print("Electronic count is : " + str(electronic_count))
+	print("Folk count is : " + str(folk_count))
+	print("Hip-Hop count is: " + str(hiphop_count))
+	print("Indie count is : " + str(indie_count))
+	print("Jazz count is : " + str(jazz_count))
+	print("Metal count is : " + str(metal_count))
+	print("Pop count is : " + str(pop_count))
+	print("R&B count is : " + str(rb_count))
+	print("Total number of songs: " + str(total_count))
 	print("Not Rock Count is: " + str(not_rock_count))
 	print("Not Available Count is " + str(NA_count))
 	print("Other count is " + str(other_count))
